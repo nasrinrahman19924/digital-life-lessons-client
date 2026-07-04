b515002e05198daac434fd08eefc115cf7783d3d
nasrinrahman19924 <nasrinrahman19924@gmail.com>
2026-07-05 01:44:35 +0600

    Fix QueryClientProvider

--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -4,29 +4,40 @@ import { createContext, useEffect, useState } from "react";
 import axios from "axios";
 import { authClient } from "@/lib/auth-client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { HeroUIProvider } from "@heroui/react";
+
 export const UserContext = createContext();
 
 export default function UserProvider({ children }) {
   const [userInfo, setUserInfo] = useState(null);
+
+  // React Query Client
+  const [queryClient] = useState(() => new QueryClient());
 
   const { data: session } = authClient.useSession();
 
   useEffect(() => {
     if (session?.user?.email) {
       axios
-        .get(
-          `${process.env.BETTER_AUTH_URL}/api/users/${session.user.email}`,
-        )
-
+        .get(`${process.env.NEXT_PUBLIC_API_URL}/users/${session.user.email}`)
         .then((res) => {
           setUserInfo(res.data);
+        })
+        .catch((err) => {
+          console.error(err);
         });
     }
   }, [session]);
 
   return (
-    <UserContext.Provider value={{ userInfo, setUserInfo }}>
-      {children}
-    </UserContext.Provider>
+    <HeroUIProvider>
+      <QueryClientProvider client={queryClient}>
+        <UserContext.Provider value={{ userInfo, setUserInfo }}>
+          {children}
+        </UserContext.Provider>
+      </QueryClientProvider>
+    </HeroUIProvider>
   );
 }
