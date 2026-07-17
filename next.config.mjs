6d481fd1bbff2516ae499aa5d711642c8d921bf6
nasrinrahman19924 <nasrinrahman19924@gmail.com>
2026-07-17 18:33:04 +0600

    create landing page ui

--- next.config.mjs
+++ next.config.mjs
@@ -4,41 +4,39 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "png.pngtree.com",
+        hostname: "i.ibb.co",
+        port: "",
+        pathname: "/**", // 🚨 এটি নিশ্চিত করবে i.ibb.co-এর ভেতরের সব ফোল্ডার/ইমেজ যেন এলাউড হয়
       },
       {
         protocol: "https",
-        hostname: "i.ibb.co.com",
+        hostname: "png.pngtree.com",
       },
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
       },
-       {
+      {
         protocol: "https",
         hostname: "www.commonsense.org",
       },
-       {
+      {
         protocol: "https",
         hostname: "kommodo.ai",
       },
-       {
-        protocol: "https",
-        hostname: "ibb.co.com",
-      },
-       {
+      {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
-       {
+      {
         protocol: "https",
         hostname: "cdn2.psychologytoday.com",
       },
-       {
+      {
         protocol: "https",
         hostname: "i0.wp.com",
       },
-       {
+      {
         protocol: "https",
         hostname: "cdn.prod.website-files.com",
       },
